6952b95a21a9ebbb6e30c74d7ca9875ef0824620
Masoud Bigdeli <masoud8bigdeli@gmail.com>
2022-04-21 21:36:11 +0430

    commit-number-2

--- src/components/OtherBooks.js
+++ src/components/OtherBooks.js
@@ -7,7 +7,9 @@ function OtherBooks(props) {
         <img src={props.image} width="150px" />
       </div>
       <div class="other-book-name">{props.bookName}</div>
-      <div class="other-book-writer">{props.bookWriter}</div>
+      <div class="other-book-writer text-overflow-3dots-130">
+        {props.bookWriter}
+      </div>
     </div>
   );
 }
